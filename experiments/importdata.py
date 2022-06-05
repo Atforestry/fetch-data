f054bb86b30f979257680f71b3ca7af1bf0a0c47
Borja Gómez Muñoz <borja@reverscore.com>
2022-06-05 22:25:56 +0200

    Update insert sql

--- experiments/importdata.py
+++ experiments/importdata.py
@@ -1,13 +1,17 @@
 import pandas as pd
 import psycopg2
+from sqlalchemy import create_engine
 
-conn_string = f'postgresql://postgres:postgres@db:5432/atforestry'
-db = create_engine(conn_string)
-conn = db.connect()
+#conn_string = f'postgresql://postgres:postgres@0.0.0.0:5432/atforestry'
+#db = create_engine(conn_string)
+#conn = db.connect()
         
 data = pd.read_csv('data.csv')
 
-data.to_sql('data', con=conn, if_exists='append',
-        index=False)
+sqlinsert = data.to_sql()
 
-conn.close()
+#data.to_sql('prediction', con=conn, if_exists='append',
+#        index=False)
+
+#conn.close()
+print(sqlinsert)
