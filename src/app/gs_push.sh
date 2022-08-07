4d23a5376f11370a21c9815b5d56d137ef82d7e5
Borja Gómez Muñoz <borja@reverscore.com>
2022-08-07 22:14:02 +0200

    Fix GCP

--- src/app/gs_push.sh
+++ src/app/gs_push.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-gcloud auth activate-service-account 411650743689-compute@developer.gserviceaccount.com --key-file=./google.json --project=mlops-3
+gcloud auth activate-service-account atforestry@atforestry.iam.gserviceaccount.com --key-file=./google.json --project=atforestry
 gsutil cp -r ./src/data/mosaics/ gs://atforestry-model-tracker/planet_data
