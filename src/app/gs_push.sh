2e761abbac58a06fe05cb006e08767ada50ab229
Borja Gómez Muñoz <borja@reverscore.com>
2022-08-09 14:40:06 +0200

    Debugging 5

--- src/app/gs_push.sh
+++ src/app/gs_push.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-gcloud auth activate-service-account atforestry@atforestry.iam.gserviceaccount.com --key-file=./google.json --project=atforestry
-gsutil -m cp -r ./src/data/mosaics/ gs://atforestry-model-tracker/planet_data
+gcloud auth activate-service-account atforestry@atforestry.iam.gserviceaccount.com --key-file=/app/google.json --project=atforestry && gsutil -m cp -r /app/src/data/mosaics/ gs://atforestry-model-tracker/planet_data
--- src/app/utils.py
+++ src/app/utils.py
@@ -121,7 +121,7 @@ def predict_raster_deforestation_category(path:str):
 
 def push_to_gcp():
     """Push data into GCP bucket"""
-    !gcloud auth activate-service-account atforestry@atforestry.iam.gserviceaccount.com --key-file=/app/google.json --project=atforestry && gsutil -m cp -r /app/src/data/mosaics/ gs://atforestry-model-tracker/planet_data
+    subprocess.call(["/app/src/app/gs_push.sh"])
 
 
 
